f62397a5ac9a7d54c9caf88bb7bc15391ed7296a
Adrian Wersching <adrian.wersching@logicline.de>
2018-05-02 09:26:50 +0200

    Refactor to library

--- src/main/java/fruitbagger/client/Fruit.java
+++ src/main/java/fruitbagger/client/Fruit.java
@@ -1,11 +1,28 @@
 package fruitbagger.client;
 
 public class Fruit {
-    public Integer index;
-    public Integer weight;
+
+    private Integer index;
+    private Integer weight;
 
     public Fruit(Integer index, Integer weight) {
         this.index = index;
+        this.weight = weight;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
         this.weight = weight;
     }
 }
